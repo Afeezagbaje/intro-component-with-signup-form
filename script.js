d24e7cc736f981981e6134394c022c5f5ebccca6
Afeez <afeezagbaje@gmail.com>
2021-02-01 16:08:33 +0100

    before breaking my codes

--- script.js
+++ script.js
@@ -4,22 +4,36 @@ const email = document.getElementById('email-address');
 const password = document.getElementById('password');
 const button = document.getElementById('claim');
 const warns = document.querySelectorAll('.warn');
-const inputs = [firstName, lastName, email, password];
+const inputs = [firstName, lastName, password];
 
 function validate() {
     for(input of inputs){
         if(input.value == ''){
-            let inside =  input.name + " cannot be empty"; 
-            input.nextElementSibling.textContent = inside;
-            input.setAttribute("required", "");
-            input.focus();
+            changeBox(input)
             return false;   
-        } else if(input.value !== ''){
-            input.removeAttribute("required", ""); 
-            input.nextElementSibling.textContent = '';              
         } 
     };
+    if(email.value.includes("@") == false || email.value.includes(".com") == false){
+        // email.setAttribute("placeholder", "email@example/com")
+        emailBox(email)
+        return false;   
+    }
 };
+
+function changeBox (a){
+    let inside =  input.name + " cannot be empty"; 
+    a.nextElementSibling.textContent = inside;
+    a.setAttribute("required", "");
+    a.focus();
+}
+function emailBox(a){
+    email.setAttribute("placeholder", "email@example/com")
+    a.nextElementSibling.textContent = " Looks like this is not an email";
+    // place.style.color = 'hsl(0, 100%, 74%)';
+    a.setAttribute("required", "");
+    a.focus();
+}
+
 
     // if(email.value.includes("@") == false || email.value.includes(".com") == false){
     //     email.nextElementSibling.textContent = 'Looks like this is not an email';
