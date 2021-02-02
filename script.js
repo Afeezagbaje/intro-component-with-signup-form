43765cbed699424fcf1d40409207f6d1a5ebda4e
Afeez <afeezagbaje@gmail.com>
2021-02-02 16:05:36 +0100

    added comment to the javascript file

--- script.js
+++ script.js
@@ -7,24 +7,30 @@ const warns = document.querySelectorAll('.warn');
 const inputs = [firstName, lastName, password];
 
 function validate() {
+// i added firstName , lastName and password into an array called inputs, 
+// so as to loop through them instead of typing or applying the code one by one.
+// email was seperated because it's going to have a different wordings and placeholder when left blank 
     for(input of inputs){
         if(input.value == ''){
             changeBox(input)
             return false;   
         } 
     };
+    // this is used to check if the users typed in an invalid email ie email without an @ and .
     if(email.value.includes("@") == false || email.value.includes(".") == false){
         emailBox(email)
         return false;   
     }
 };
 
+//this function is used to add required to the input tag been selected, so as to validate the input, then to add focus to the input if it's invalid and also show the warning
 function changeBox (a){
     let inside =  input.name + " cannot be empty"; 
     a.nextElementSibling.textContent = inside;
     a.setAttribute("required", "");
     a.focus();
 }
+// this is similar to changeBox but a little different because of the different warning for the email input tag
 function emailBox(a){
     email.setAttribute("placeholder", "email@example/com")
     a.nextElementSibling.textContent = " Looks like this is not an email";
